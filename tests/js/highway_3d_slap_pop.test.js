a690818169ccb011503ab10d19bf7797e5ab417d
Viktor Olausson <viktor.olausson@gmail.com>
2026-09-18 15:07:02 +0200

    fix(highway): join linked vibrato trails smoothly

--- tests/js/highway_3d_slap_pop.test.js
+++ tests/js/highway_3d_slap_pop.test.js
@@ -61,6 +61,7 @@ test('optional attack flags do not leak from one reused chord member to the next
     assert.ok(start > 0 && end > start);
     const copy = new Function(`
         const _linkedBendStarts = new WeakMap(), _linkedBendEnds = new WeakMap();
+        const _linkedVibratoRuns = new WeakMap();
         return function(_scrChordNote, cn, ch) {
             ${src.slice(start, end)}
             return _scrChordNote;
